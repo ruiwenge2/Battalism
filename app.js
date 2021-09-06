4a9596453f5462fd1c3073f27f7654769ff6ca2d
ruiwenge2 <85425620+ruiwenge2@users.noreply.github.com>
2021-09-06 15:23:26 +0000

    edit app.js

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
+app.use(express.static("public"));
 
 const socketfunc = require("./game/socketfunction");
 const update = require("./game/update");
 
 global.io = io;
 global.users = {};
-app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
